cfa4108e55c91df50d0e5614e5a23c7cd9a7c84d
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-09-29 08:52:38 -0300

    chore: added map libs and axios.

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URI;
+
+const methods = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  patch: axios.patch,
+};
+
+export function useAxios({
+  path = "",
+  method = "",
+  config = null,
+  query = "",
+}) {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const axiosMethod = async () => {
+    try {
+      setLoading(true);
+      const axios = methods[method];
+      const response = await axios(
+        `${BASE_URL}/${path}${query}`,
+        config?.[0],
+        config?.[1]
+      );
+      setResponse(response);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+      setError(null);
+      setResponse(null);
+    }
+  };
+
+  useEffect(() => {
+    if (method !== "") {
+      axiosMethod();
+    }
+    // eslint-disable-next-line
+  }, [path, method, config, query]);
+
+  return { response, error, loading };
+}
