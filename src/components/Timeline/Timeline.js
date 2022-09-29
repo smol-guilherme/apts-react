555082e79fd777e99d4fb4f7c6dbaaa07e2cfe11
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-09-29 09:38:10 -0300

    feat: added skeleton main page and map.

---
+++ src/components/Timeline/Timeline.js
@@ -0,0 +1,10 @@
+import MapOverlay from "../Map/Map.js";
+import { Content } from "../shared/pageBody.jsx";
+
+export default function Timeline() {
+  return (
+    <Content>
+      <MapOverlay />
+    </Content>
+  );
+}
