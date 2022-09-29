52e3c2ead43adfc17fd53c490e6a28aaec484298
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-09-29 09:37:44 -0300

    style: added generic main body element for components.

---
+++ src/components/shared/pageBody.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 80vh;
+  /* background-color: #333333; */
+  color: white;
+`
