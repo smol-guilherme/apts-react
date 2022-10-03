e374a3724771e3ec424469bc7afcd49908f2a879
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-10-03 11:37:15 -0300

    feat: added 'new review' button at the bottom of the page.

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,8 +1,11 @@
 import { useContext } from "react";
+import { TiThMenu } from "react-icons/ti";
+import { TbComet } from "react-icons/tb";
+import { MdStars } from "react-icons/md";
+import { Content, Header, NewEntry } from "../shared/pageBody.jsx";
+import { Post } from "../shared/postBody.jsx";
 import DataContext from "../context/DataContext.js";
 import MapOverlay from "../Map/Map.js";
-import { Content, Header } from "../shared/pageBody.jsx";
-import { Post } from "../shared/postBody.jsx";
 
 const lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -28,10 +31,33 @@ export default function Timeline() {
     return <Post handleClick={handleClick} {...data} />;
   };
 
+  const HeaderElement = () => {
+    return (
+      <Header>
+        <div>
+          <TiThMenu />
+        </div>
+        <div>
+          A PLACE TO STAR <TbComet />
+        </div>
+        <div></div>
+      </Header>
+    );
+  };
+
+  const BottomElement = () => {
+    return (
+      <NewEntry>
+        <MdStars />
+      </NewEntry>
+    );
+  };
+
   return (
     <Content>
-      <Header>A PLACE TO STAR</Header>
+      <HeaderElement />
       <Render />
+      <BottomElement />
     </Content>
   );
 }
--- src/components/shared/pageBody.jsx
+++ src/components/shared/pageBody.jsx
@@ -6,6 +6,7 @@ export const Content = styled.div`
   width: 100vw;
   height: 80vh;
   padding-top: 10vh;
+  flex-grow: 1;
   /* background-color: #333333; */
   justify-content: center;
   margin: 0 auto;
@@ -19,7 +20,27 @@ export const Header = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  background-color: aqua;
+  background-color: #70b1e9;
+  font-size: 30px;
   box-shadow: 0px 2px 3px 2px #66666650;
   z-index: 1000;
+  justify-content: space-between;
+  
+  div {
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+  }
+
+  :nth-child(2) {
+    flex-direction: column;
+  }
+`
+
+export const NewEntry = styled.div`
+  position: fixed;
+  bottom: 3.25vw;
+  right: 2.25vh;
+  font-size: 56px;
+  color: #70b1e9;
 `
